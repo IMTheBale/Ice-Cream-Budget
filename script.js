3647e100a10ab7c023dc0fa31ba0a29dc2b42196
IMTheBale <johnny.bale.play@gmail.com>
2023-10-04 13:57:01 +0300

    created script

---
+++ script.js
@@ -0,0 +1,13 @@
+let price = 2.50;
+let campers = 15;
+console.log(price);
+console.log(campers);
+let taxRate = 0.08;
+console.log(taxRate);
+let budget = 40;
+let total = price * campers * (1 + taxRate);
+console.log(total);
+let totalExceedsBudget = total > budget;
+console.log("Total Exceeds Budget? " + totalExceedsBudget);
+let taxApplied = total > (price * campers);
+console.log("Tax Applied? " + taxApplied);
